c3797621230246ed575bf1486ed7e5a77b6f363d
anthony-vo <s3951749@student.rmit.edu.au>
2025-05-27 11:09:07 +1000

    Config file for database connection

---
+++ partfinder/src/backend/config.php
@@ -0,0 +1,23 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+// Load .env
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+// Set variables
+$host = $_ENV['DB_HOST'];
+$databse = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASS'];
+
+$dsn = "mysql:host={$host};dbname={$database};charset=utf8mb4";
+
+// using PDO insteal of mysqlcli
+try {
+  $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,]);
+} catch (PDOEXCEPTION $e) {
+  http_response_code(500);
+  echo json_encode(['Error' => 'Database connection failed']);
+  exit;
+}
